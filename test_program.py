6330a941673e568712cc7d90c4a1ce0c107b4fb7
Zubakhina Tatyana <33422677+tanya525625@users.noreply.github.com>
2020-05-02 13:04:08 +0300

    added test

--- test_program.py
+++ test_program.py
@@ -6,8 +6,9 @@ def test_recognizer():
     rec = EmotionsRecognizer()
     test_path = os.path.join(os.getcwd(), "files_for_tests")
     predictions = []
-    right_emotions = ['happiness', 'surprise', 'disgust']
+    right_emotions = []
     for file in os.listdir(test_path):
+        right_emotions.append(file.split('.')[0])
         predictions.append(rec.predict(os.path.join(test_path, file)))
 
     assert predictions == right_emotions
