f5dec48861ada0a9f73ea386189780eec19e5e69
Bainova Ekaterina <ekaterinabainova@gmail.com>
2020-05-26 11:31:39 +0300

    Update export_virtvars.sh

--- scripts/export_virtvars.sh
+++ scripts/export_virtvars.sh
@@ -1,5 +1,6 @@
 export CLOUD_SQL_CONNECTION_NAME='smartyad:europe-west2:statistic'
 export DB_USER='root'
-export DB_PASS='89041611139Maslman'
+echo "write password to the DB_PASS"
+export DB_PASS=''
 export DB_NAME='smartadstats'
 export BOARD_ID=1
